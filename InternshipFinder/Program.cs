85365b60f575c291c0f1617b7fbad9e231dc0781
Rozario <sodienrozario@gmail.com>
2024-12-11 01:24:20 +0200

    able to get h4 and a tags for IT internships

---
+++ InternshipFinder/Program.cs
@@ -0,0 +1,50 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace InternshipFinder
+{
+    internal class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            string url = "https://www.graduates24.com/internshipprogrammes?page=3";
+            string html = await GetPageAsync(url);
+            ParseHtml(html);
+            Console.ReadLine();
+        }
+
+        //Fetching the page's HTML
+        public static async Task<string> GetPageAsync(string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        //Parse HMTL to extract required data
+        public static void ParseHtml(string html)
+        {
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+            var nodesh = document.DocumentNode.SelectNodes("//h4");
+            foreach (var node in nodesh)
+            {
+                if (node.InnerText.Contains("IT"))
+                {
+                    HtmlNode firstChild = node.SelectSingleNode(".//a[@href]");
+
+                    Console.WriteLine(node.InnerText);
+                    Console.WriteLine(firstChild.GetAttributeValue("href", string.Empty));
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
